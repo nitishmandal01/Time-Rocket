8d140b209819223532b387a97d10804a14ac50a8
nitishmandal01 <neelgiri65@gmail.com>
2022-12-18 04:15:26 +0530

    all-done

---
+++ components/navbar.js
@@ -0,0 +1,29 @@
+
+    const navbar=()=>{
+        return `
+    <div id="nav">
+       <div id="nab">
+           <div id="logo">
+            <img src="C:\Users\dell\Downloads\TIME-ROCKET-removebg-preview.png" alt="">
+           </div>
+           <div id="menu">
+               <a class="active" href="">Demo</a>
+               <a  class="active1" href="">Feature</a>
+               <a  class="active2" href="">Pricing</a>
+               <a href="">About us</a>
+               <a href="">FAQ</a>
+               <a href="">Blog</a>
+                <button id="btn">LOGIN</button>
+                <button id="sign">SIGN UP</button>
+                <select name="" id="select">
+                    
+                    <option value="">English</option>
+                    <option value="">Espanol</option>
+                </select>
+              
+           </div>
+       </div>
+    </div>`
+    }
+
+    export {navbar}
---
+++ index.js
@@ -0,0 +1,10 @@
+import { navbar } from "./components/navbar.js"; 
+
+let navbar_div=document.getElementById("navbar");
+navbar_div.innerHTML=navbar();
+
+
+// import { footer } from "./components/footer.js";
+
+// let footer_part = document.getElementById("footer-part")
+// footer_part.innerHTML  = footer()
